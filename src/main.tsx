9bd63bead01d5deec123b5a674381bc0f7eeafa3
ArnavPundir22 <arnavp128@gmail.com>
2026-07-05 15:01:51 +0530

    fix: change appearance property to theme

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ createRoot(document.getElementById('root')!).render(
       afterSignOutUrl="/"
       signInFallbackRedirectUrl="/"
       signUpFallbackRedirectUrl="/"
-      appearance={{ baseTheme: dark }}
+      appearance={{ theme: dark }}
     >
       <App />
     </ClerkProvider>

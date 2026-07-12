18a6d2d1da227f2868b30c242dfc7e2e10451508
ArnavPundir22 <arnavp128@gmail.com>
2026-07-12 13:56:12 +0530

    fix: remove manual registerSW to prevent auto-update infinite loops

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { registerSW } from 'virtual:pwa-register'
-
-// Automatically reload the page when a new version of the app is deployed
-// This prevents the "missing CSS" caching issues in production.
-registerSW({
-  immediate: true,
-  onNeedRefresh() {
-    // A new update is available, force the browser to reload and get the new CSS/JS
-    window.location.reload();
-  }
-});
 
 // Automatic Data Reset for v4 Launch
 if (localStorage.getItem('zerolag_reset_epoch') !== 'v4') {
